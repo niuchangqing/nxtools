c976c2eeb15f5ffd642642352127d938e5b45800
niuchangqing <ncqxwh@163.com>
2021-10-23 23:03:06 +0800

    修改PasswordEncoder bean

--- jwt-boot-starter/src/main/java/cn/nxtools/jwt/autoconfigure/JwtAutoConfiguration.java
+++ jwt-boot-starter/src/main/java/cn/nxtools/jwt/autoconfigure/JwtAutoConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author ncq
@@ -69,7 +70,7 @@ public class JwtAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
