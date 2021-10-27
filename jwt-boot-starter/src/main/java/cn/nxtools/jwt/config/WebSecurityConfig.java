fae59fe32f4da403c41b06dced7d557f875185a8
niuchangqing <ncqxwh@163.com>
2021-10-28 00:53:04 +0800

    修改使用注入BCryptPasswordEncoder错误

--- jwt-boot-starter/src/main/java/cn/nxtools/jwt/config/WebSecurityConfig.java
+++ jwt-boot-starter/src/main/java/cn/nxtools/jwt/config/WebSecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static cn.nxtools.common.StringUtil.isEmpty;
@@ -36,7 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private String[] extralPermitUrls = null;
 
     @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private AuthenticationAccessDeniedHandler authenticationAccessDeniedHandler;
@@ -82,6 +82,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // 设置UserDetailsService
                 .userDetailsService(customUserDetailsService)
                 // 使用BCrypt进行密码的hash
-                .passwordEncoder(bCryptPasswordEncoder);
+                .passwordEncoder(passwordEncoder);
     }
 }
