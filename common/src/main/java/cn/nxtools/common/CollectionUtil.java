e86695b13d282c51a1c9358c41146df6da3e8720
niuchangqing <ncqxwh@163.com>
2022-03-13 13:51:12 +0800

    add list to map method

--- common/src/main/java/cn/nxtools/common/CollectionUtil.java
+++ common/src/main/java/cn/nxtools/common/CollectionUtil.java
@@ -1,7 +1,10 @@
 package cn.nxtools.common;
 
+import cn.nxtools.common.collect.Maps;
+
 import java.util.Collection;
 import java.util.Map;
+import java.util.function.Function;
 
 /**
  * @author niuchangqing
@@ -118,5 +121,65 @@ public class CollectionUtil {
      */
     public static boolean isNotEmpty(Map<?, ?> map) {
         return map != null && !map.isEmpty();
+    }
+
+    /**
+     * 集合转Map, 指定集合对象中的字段作为key或value <br>
+     * <pre>
+     *     List<User> list = Lists.newArrayList();
+     *     User user = new User();
+     *     user.setId(1L);
+     *     list.add(user);
+     *     Map<Long, User> map = CollectionUtil.toMap(map, list, User::getId, v -> v);
+     * </pre>
+     * @param iterable          集合
+     * @param key               map的key
+     * @param value             map的value
+     * @param <T>               T
+     * @param <K>               K
+     * @param <V>               V
+     * @return                  转换后的结果Map
+     * @since 1.0.4
+     */
+    public static <T, K, V> Map<K, V> toMap(Iterable<T> iterable, Function<T, K> key, Function<T, V> value) {
+        return toMap(Maps.newHashMap(), iterable, key, value);
+    }
+
+    /**
+     * 集合转Map, 指定集合对象中的字段作为key或value <br>
+     * 如果map为空，会调用Maps.newHashMap();方法初始化map
+     * <pre>
+     *     List<User> list = Lists.newArrayList();
+     *     User user = new User();
+     *     user.setId(1L);
+     *     list.add(user);
+     *     Map<Long, User> map = Maps.newHashMap();
+     *     map = CollectionUtil.toMap(map, list, k -> k.getId(), v -> v);
+     * </pre>
+     * @param map               转换的Map
+     * @param iterable          集合
+     * @param key               key
+     * @param value             value
+     * @param <T>               T
+     * @param <K>               K
+     * @param <V>               V
+     * @return                  转换后的结果Map
+     * @since 1.0.4
+     */
+    public static <T, K, V> Map<K, V> toMap(Map<K, V> map, Iterable<T> iterable, Function<T, K> key, Function<T, V> value) {
+        if (iterable == null) {
+            return map;
+        }
+        if (map == null) {
+            map = Maps.newHashMap();
+        }
+        for (T t : iterable) {
+            if (t == null) {
+                map.put(null, null);
+            } else {
+                map.put(key.apply(t), value.apply(t));
+            }
+        }
+        return map;
     }
 }
--- common/src/test/java/cn/nxtools/common/CollectionUtilTest.java
+++ common/src/test/java/cn/nxtools/common/CollectionUtilTest.java
@@ -1,8 +1,65 @@
 package cn.nxtools.common;
 
+import cn.nxtools.common.collect.Lists;
+import cn.nxtools.common.collect.Maps;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author niuchangqing
  */
 public final class CollectionUtilTest {
+    @Test
+    public void testToMap() {
+        List<CollectionUtilTest.User> list = Lists.newArrayList();
+        list.add(new User(1L, "张三"));
+        list.add(new User(2L, "李四"));
+        Map<Long, User> map = Maps.newHashMap();
+        map = CollectionUtil.toMap(map, list, User::getId, user -> user);
+        System.out.println(JsonUtil.toString(map));
+        //结果字符串: {"1":{"id":1,"name":"张三"},"2":{"id":2,"name":"李四"}}
+    }
 
+    @Test
+    public void testToMap1() {
+        List<CollectionUtilTest.User> list = Lists.newArrayList();
+        list.add(new User(1L, "张三"));
+        list.add(new User(2L, "李四"));
+        Map<Long, User> map = CollectionUtil.toMap(list, k -> k.getId(), v -> v);
+        System.out.println(JsonUtil.toString(map));
+        //结果字符串: {"1":{"id":1,"name":"张三"},"2":{"id":2,"name":"李四"}}
+    }
+
+
+    public static class User {
+        private Long id;
+
+        private String name;
+
+        public User() {
+        }
+
+        public User(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
